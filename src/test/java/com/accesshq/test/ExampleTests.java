83bce2270f941fd0a7ba108420c28bc898cfb5e7
Dan Humphrey <dan.humphrey@accesshq.com>
2019-04-09 13:01:54 +1000

    Update tests

--- src/test/java/com/accesshq/test/ExampleTests.java
+++ src/test/java/com/accesshq/test/ExampleTests.java
@@ -12,7 +12,7 @@ import com.accesshq.util.Config;
 class ExampleTests extends BaseTestSuite {
 
 	@Test
-	void test1() {
+	void testHomePageHeading() {
 		WebDriver driver = getDriver();
 		driver.get(Config.APP_URL);
         WebElement h1Element = driver.findElement(By.tagName("h1"));
@@ -20,26 +20,28 @@ class ExampleTests extends BaseTestSuite {
 	}
 
 	@Test
-	void test2() {
+	void testCareersPageHeading() {
 		WebDriver driver = getDriver();
 		driver.get(Config.APP_URL);
+        driver.findElement(By.linkText("Careers")).click();
         WebElement h1Element = driver.findElement(By.tagName("h1"));
-        Assertions.assertEquals("Human Quality ... because people make technology", h1Element.getText());
+        Assertions.assertEquals("Careers", h1Element.getText());
 	}
 	
 	@Test
-	void test3() {
+	void testAboutPageHeading() {
 		WebDriver driver = getDriver();
 		driver.get(Config.APP_URL);
+        driver.findElement(By.linkText("About us")).click();
         WebElement h1Element = driver.findElement(By.tagName("h1"));
-        Assertions.assertEquals("Human Quality ... because people make technology", h1Element.getText());
+        Assertions.assertEquals("About Us", h1Element.getText());
 	}
 	
 	@Test
-	void test4() {
+	void testFailingHomePageHeading() {
 		WebDriver driver = getDriver();
 		driver.get(Config.APP_URL);
         WebElement h1Element = driver.findElement(By.tagName("h1"));
-        Assertions.assertEquals("Human Quality ... because people make technology", h1Element.getText());
+        Assertions.assertEquals("Quality Humans ... because people make technology", h1Element.getText());
 	}
 }
