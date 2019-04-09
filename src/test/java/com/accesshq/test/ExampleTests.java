56e614d6653351eb4a97b662abce232cb78aa94c
Dan Humphrey <dan.humphrey@accesshq.com>
2019-04-10 08:32:38 +1000

    Update unused imports

--- src/test/java/com/accesshq/test/ExampleTests.java
+++ src/test/java/com/accesshq/test/ExampleTests.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import com.accesshq.util.Config;
-
 
 class ExampleTests extends BaseTestSuite {
 
